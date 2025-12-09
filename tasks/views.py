b46eb829c09cd7712362ef4111e127be22056790
BearForOdin <selasidzeamesi82@gmail.com>
2025-12-08 19:59:00 -0700

    allowed

--- tasks/views.py
+++ tasks/views.py
@@ -2,10 +2,19 @@ from rest_framework import viewsets, filters
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.utils import timezone
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Task
 from .serializers import TaskSerializer, TaskStatusSerializer
 from .permissions import IsOwner
+
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
